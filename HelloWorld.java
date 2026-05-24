2cc6776e47ba25a6899fbfce7d332fb68232b926
Sebastian Soza Madariaga <sebastian.soza.m@mail.pucv.cl>
2026-05-24 16:50:58 -0400

    primeros pasos

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+buenassss
+*/
+
+//hola mundo
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Repollo");
+    }
+}
---
+++ VariablesConstantes.java
@@ -0,0 +1,27 @@
+public class VariablesConstantes {
+    static void main() {
+        //Variables
+
+        String name = "Sebastian";
+        System.out.println(name);
+
+        int age = 24;
+
+        name = "Repollo";
+
+        System.out.println(age);
+        System.out.println(name);
+
+        var email = "sebaa.soza@gmail.com";
+        System.out.println(email);
+
+        var year = 2026;
+        System.out.println(email);
+
+        //Constantes
+
+        final String EMAIL = "sebaa.soza@gmail.com";
+        System.out.println(EMAIL);
+
+    }
+}
